b6caa1ea19fcbdbb4530f1559044b63b8eb77dab
mlee3142 <michaellee@dhcp-18-189-73-116.dyn.mit.edu>
2016-09-05 11:25:47 -0400

    Working 4-way
    4-way tutorial phase and main phase are working

--- experiments/mutator_SR/driver_mutatorSR.py
+++ experiments/mutator_SR/driver_mutatorSR.py
@@ -9,29 +9,29 @@ import dldata.stimulus_sets.hvm as hvm
 from mturkutils.base import MatchToSampleFromDLDataExperiment
 from os import path
 import json
-REPEATS_PER_QE_IMG = 4
-ACTUAL_TRIALS_PER_HIT = 140
-
+REPEATS_PER_QE_IMG = 4 # Number of times to repeat select images, for "Quality Estimation" purposes
+ACTUAL_TRIALS_PER_HIT = 140 # Number of experimental trials, without repeat images. And without tutorial trials.
 
 def get_url_labeled_resp_img(cat):
+    # Get canonical response image for this 'category' cat.
     base_url = 'https://canonical_images.s3.amazonaws.com/'
     return base_url+ cat+ '.png'
 
 
-def get_exp(sandbox=True, debug=False, dummy_upload=True):
-    dataset = hvm.HvMWithDiscfade()
-    meta = dataset.meta
+def get_exp(sandbox=True, debug=True, dummy_upload=True):
+    dataset = hvm.HvMWithDiscfade() # Pull urls and meta from this dldata dataset.
+    meta = dataset.meta # Load meta.
     response_images = []
-    categories = np.unique(meta['category'])
-    cat_combs= [e for e in itertools.combinations(categories, 2)]
+    categories = np.unique(meta['category']) # The categories for this experiment are the ones in meta.
+    cat_combs= [e for e in itertools.combinations(categories, 4)] # Get every possible pairing of categories; these are the confusions to be measured.
     response_images.extend([{
-    'urls': [get_url_labeled_resp_img(c1), get_url_labeled_resp_img(c2)],
-    'meta': [{'category': category} for category  in [c1, c2]],
-    'labels': [c1, c2]
-    } for c1, c2 in cat_combs])
+    'urls': [get_url_labeled_resp_img(c1), get_url_labeled_resp_img(c2), get_url_labeled_resp_img(c3), get_url_labeled_resp_img(c4)],
+    'meta': [{'category': category} for category in [c1, c2, c3, c4]],
+    'labels': [c1, c2, c3, c4]
+    } for c1, c2, c3, c4 in cat_combs]) # List of dicts for response images information; one entry (a dict)_ for each confusion to be measured.
     combs = cat_combs
 
-    urls = dataset.publish_images(range(len(dataset.meta)), None, 'hvm_timing',
+    urls = dataset.publish_images(range(len(dataset.meta)), None, 'hvm_timing', # dldata method that uploads to s3 the images, in their own bucket.
                                       dummy_upload=dummy_upload)
 
     with open(path.join(path.dirname(__file__), 'tutorial_html_basic'), 'r') as tutorial_html_file:
@@ -40,7 +40,7 @@ def get_exp(sandbox=True, debug=False, dummy_upload=True):
     html_data = {
         'combs': combs,
         'response_images': response_images,
-        'num_trials': 125 * 2,
+        'num_trials': 125 * 2, # "Number of times to measure each confusion." I think this means total times over the population
         'meta_field': 'category',
         'meta': meta,
         'urls': urls,
@@ -67,16 +67,16 @@ def get_exp(sandbox=True, debug=False, dummy_upload=True):
             htmlsrc='web/general_mutatorSR.html',
             htmldst='hvm_mutatorSR_n%05d.html',
             sandbox=sandbox,
-            title='Noun learning --- match strange objects to hieroglyphs',
+            title='Visual object learning --- match strange objects to hieroglyphs',
             reward=0.25,
             duration=1600,
             keywords=['neuroscience', 'psychology', 'experiment', 'object recognition'],  # noqa
             description="***You may complete as many HITs in this group as you want*** Complete a visual object recognition task where you report the identity of objects you see. We expect this HIT to take about 10 minutes or less, though you must finish in under 25 minutes.  By completing this HIT, you understand that you are participating in an experiment for the Massachusetts Institute of Technology (MIT) Department of Brain and Cognitive Sciences. You may quit at any time, and you will remain anonymous. Contact the requester with questions or concerns about this experiment.",  # noqa
             comment='mutatorSR',
-            collection_name='hvm_2ways', # database
-            max_assignments=1,
-            bucket_name='hvm_2ways', # amazon ref
-            trials_per_hit=ACTUAL_TRIALS_PER_HIT + 24,  # 140 + 6x4 repeats
+            collection_name= None,#'hvm_basic_2ways', # name of MongoDB db to update / create on dicarlo5. Safe to change?
+            max_assignments=1, # how many unique workers can complete a particular hit.
+            bucket_name='hvm_2ways', # Refers to amazon; either mturk or s3 service (I'm not sure.) on which to store htmls. not safe to change before uploading source to bucket
+            trials_per_hit= ACTUAL_TRIALS_PER_HIT + 24,  # ACTUAL_TRIALS_PER_HIT + (6xREPEATS_PER_QE_IMG) repeats
             html_data=html_data,
             tmpdir='tmp',
             frame_height_pix=1200,
@@ -87,7 +87,8 @@ def get_exp(sandbox=True, debug=False, dummy_upload=True):
     # -- create trials
     exp.createTrials(sampling='with-replacement', verbose=1)
     n_total_trials = len(exp._trials['imgFiles'])
-    assert n_total_trials == (8 * 7 / 2) * 250
+    print n_total_trials
+   # assert n_total_trials == (70) * 250 # Number of confusions * number of times each confusion is to be measured html_data['num_trials']
     if debug:
         return exp, html_data
 
@@ -153,7 +154,7 @@ def get_exp(sandbox=True, debug=False, dummy_upload=True):
     return exp, html_data
 
 
-def main(argv=[], partial=True, debug=False):
+def main(argv=[], partial=True, debug=True):
     sandbox = True
     if len(argv) > 1 and argv[1] == 'production':
         sandbox = False
